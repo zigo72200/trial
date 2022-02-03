56f0b86e9fe4446a1f04831867089a58c247f2b7
zigo72200 <91802848+zigo72200@users.noreply.github.com>
2022-02-03 17:58:29 +0200

    заміна

--- js/main.js
+++ js/main.js
@@ -23,31 +23,38 @@ const atTheOldToad = {
   },
 
   removePotion(potionName) {
-    const potionIndex = this.potions.indexOf(potionName);
-    
     for (let i = 0; i < this.potions.length; i += 1) {
       if (this.potions[i].name === potionName.name) {
-        return potionIndex = i;
+        this.potions.splice(i, 1);
+
+        return `Potion ${potionName} remove`;
       }
     }
-
-    if (potionIndex === -1) {
       return `Potion ${potionName} is not in inventory!`;
-    }
 
-    this.potions.splice(potionIndex, 1);
+    
   },
 
   updatePotionName(oldName, newName) {
-    const potionIndex = this.potions.indexOf(oldName);
+    for (let i = 0; i < this.potions.length; i += 1) {
+      if (this.potions[i].name === oldName) {
+        this.potions[i].name = newName;
 
-    if (potionIndex === -1) {
-      return `Potion ${oldName} is not in inventory!`;
+       
+      }
     }
+      return `Potion ${oldName} is not in inventory!`;
 
-    this.potions.splice(potionIndex, 1, newName);
+    // const potionIndex = this.potions.indexOf(oldName);
+
+    // if (potionIndex === -1) {
+    //   return `Potion ${oldName} is not in inventory!`;
+    // }
+
+    // this.potions.splice(potionIndex, 1, newName);
   },
   // Change code above this line
 };
-atTheOldToad.addPotion({ name: "Speed potion", price: 460 })
-console.log(atTheOldToad.potions)
+
+atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion")
+console.log(atTheOldToad.potions);
