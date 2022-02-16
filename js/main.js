2fcbfd3e499a6ab5be280cbaa54f36c8c13b498e
zigo72200 <91802848+zigo72200@users.noreply.github.com>
2022-02-16 19:00:20 +0200

    1 - 2 задача

--- js/main.js
+++ js/main.js
@@ -1,60 +1,33 @@
-const atTheOldToad = {
-  potions: [
-    { name: "Speed potion", price: 460 },
-    { name: "Dragon breath", price: 780 },
-    { name: "Stone skin", price: 520 },
-  ],
-  // Change code below this line
-  getPotions() {
-    return this.potions;
-  },
-  addPotion(newPotion) {
-  
-    let olName = [];
-    for (const pot of this.potions) {
-      olName.push(pot.name)
-    }
-    if (olName.includes(newPotion.name)) {
-      
-      return `Error! Potion ${newPotion.name} is already in your inventory!`;
-    }
-
-    this.potions.push(newPotion);
-  },
-
-  removePotion(potionName) {
-    for (let i = 0; i < this.potions.length; i += 1) {
-      if (this.potions[i].name === potionName.name) {
-        this.potions.splice(i, 1);
-
-        return `Potion ${potionName} remove`;
-      }
-    }
-      return `Potion ${potionName} is not in inventory!`;
-
-    
-  },
-
-  updatePotionName(oldName, newName) {
-    for (let i = 0; i < this.potions.length; i += 1) {
-      if (this.potions[i].name === oldName) {
-        this.potions[i].name = newName;
-
-       
-      }
-    }
-      return `Potion ${oldName} is not in inventory!`;
-
-    // const potionIndex = this.potions.indexOf(oldName);
-
-    // if (potionIndex === -1) {
-    //   return `Potion ${oldName} is not in inventory!`;
-    // }
-
-    // this.potions.splice(potionIndex, 1, newName);
-  },
-  // Change code above this line
-};
-
-atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion")
-console.log(atTheOldToad.potions);
+class User {
+  email;
+  constructor(email) {
+    this.email = email;
+  }
+  get email() {
+    return this.email;
+  }
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class ContentEditor extends User {
+  constructor({ email, posts }) {
+    super(email);
+    this.posts = posts;
+  }
+  get posts() {
+    return this.post;
+  }
+  set posts(newPost) {
+    this.post = newPost;
+  }
+}
+const editor = new ContentEditor({ email: 'mango@mail.com', posts: [] });
+const user = new User('email@mail.com');
+console.log(user);
+console.log(editor);
+console.log(editor.email);
+editor.email = 'poly@mail.com';
+console.log(editor.email);
+editor.post = [1, 2];
+console.log(editor.post);
